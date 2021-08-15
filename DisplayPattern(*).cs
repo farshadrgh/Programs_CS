45718fa7d7d65a3240b1882c7e954ca2faecb504
Farshad Rahimi Ghashghaei <73135303+farshadrgh@users.noreply.github.com>
2021-08-15 13:19:49 -0700

    Update and rename DisplayPattern(*) to DisplayPattern(*).cs

--- DisplayPattern(*).cs
+++ DisplayPattern(*).cs
@@ -11,9 +11,9 @@ class Program
    
    Console.Write("Input number of rows : ");
    rows= Convert.ToInt32(Console.ReadLine());   
-   for(i=1;i<=rows;i++)
+   for(i=1; i<=rows; i++)
    {
-	for(j=1;j<=i;j++)
+	for(j=1; j<=i; j++)
 	   Console.Write("*");
 	Console.Write("\n");
    }
